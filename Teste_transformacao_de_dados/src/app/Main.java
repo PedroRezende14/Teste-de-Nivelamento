a50f44df9311f29ebaf5e4b6cb57289d131b40a7
PedroRezende14 <pedro.rezende14@outlook.com>
2025-03-26 08:14:40 -0300

    feat: add arquivos

--- Teste_transformacao_de_dados/src/app/Main.java
+++ Teste_transformacao_de_dados/src/app/Main.java
@@ -1,11 +1,25 @@
 package app;
 
+import java.io.IOException;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Download_Anexo1 da = new Download_Anexo1();
-		da.DownloadAnexo();
+		
+//		Download_Anexo1 da = new Download_Anexo1();
+//		da.DownloadAnexo();
+//		
+//		ExtrairTabela et = new ExtrairTabela();
+//		et.Extrair();
+//		
+		Modificador_Tabela mt= new Modificador_Tabela("tabela_extraida.csv", true, true, true, true, true);
+		try {
+			mt.processarTabelaCompleta();
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+		}
+		
 	}
 
 }
